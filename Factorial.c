c52e8ef7a18b2adbccf77aa1013c96502cb9d1bc
psykeefuego <97373191+psykeefuego@users.noreply.github.com>
2022-01-30 17:57:21 +0530

    Add files via upload

---
+++ Factorial.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char str[100];
+	printf("Enter a string \n");
+	gets(str);
+	strrev(str);
+	printf("Reverse of the string is \n %s", str);
+	return 0;
+}
